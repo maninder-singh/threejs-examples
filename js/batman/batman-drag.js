72b1a8a97678dc899dee9b25ab1267ace474551a
maninder-singh <nsit.maninder@gmail.com>
2016-06-21 15:31:44 +0530

    object move with drag event on each direction

--- js/batman/batman-drag.js
+++ js/batman/batman-drag.js
@@ -6,10 +6,14 @@
     
     function usingCSS3DRenderer(){
 
-        var targetRotation = 0;
-        var targetRotationOnMouseDown = 0;
+        var targetRotationX = 0;
+        var targetRotationY = 0;
+        var targetRotationOnMouseDownX = 0;
+        var targetRotationOnMouseDownY = 0;
         var mouseX = 0;
+        var mouseY = 0;
         var mouseXOnMouseDown = 0;
+        var mouseYOnMouseDown = 0;
         var windowHalfX = window.innerWidth / 2;
         var windowHalfY = window.innerHeight / 2;
 
@@ -67,12 +71,16 @@
             document.addEventListener( 'mouseout', onDocumentMouseOut, false );
 
             mouseXOnMouseDown = event.clientX - windowHalfX;
-            targetRotationOnMouseDown = targetRotation;
+            mouseYOnMouseDown = event.clientY - windowHalfY;
+            targetRotationOnMouseDownX = targetRotationX;
+            targetRotationOnMouseDownY = targetRotationY;
         }
 
         function onDocumentMouseMove( event ) {
             mouseX = event.clientX - windowHalfX;
-            targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+            mouseY = event.clientY - windowHalfY;
+            targetRotationX = targetRotationOnMouseDownX + ( mouseX - mouseXOnMouseDown ) * 0.02;
+            targetRotationY = targetRotationOnMouseDownY + ( mouseY - mouseYOnMouseDown ) * 0.01;
         }
 
         function onDocumentMouseUp( event ) {
@@ -91,7 +99,9 @@
             if ( event.touches.length === 1 ) {
                 event.preventDefault();
                 mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
-                targetRotationOnMouseDown = targetRotation;
+                mouseYOnMouseDown = event.touches[0].pageY - windowHalfY;
+                targetRotationOnMouseDownX = targetRotationX;
+                targetRotationOnMouseDownY = targetRotationY;
             }
         }
         
@@ -99,80 +109,32 @@
             if ( event.touches.length === 1 ) {
                 event.preventDefault();
                 mouseX = event.touches[ 0 ].pageX - windowHalfX;
-                targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+                mouseY = event.touches[0].pageY - windowHalfY;
+                targetRotationX = targetRotationOnMouseDownX + ( mouseX - mouseXOnMouseDown ) * 0.05;
+                targetRotationY = targetRotationOnMouseDownY + (mouseY - mouseYOnMouseDown) * 0.05;
             }
         }
 
         render();
             
+        var controls = {
+            rotateX : 0,
+            rotateY : 0,
+            rotateZ : 0
+        };
+
+        var gui = new dat.GUI();
+        gui.add(controls, 'rotateX', -180,180);
+        gui.add(controls, 'rotateY', -180,180);
+        gui.add(controls, 'rotateZ', -180,180);    
+
+
         function render() {
-            extrudeMesh.rotation.y += ( targetRotation - extrudeMesh.rotation.y ) * 0.05;
+            extrudeMesh.rotation.y += ( targetRotationX - extrudeMesh.rotation.y ) * 0.05;
+            extrudeMesh.rotation.x += ( targetRotationY - extrudeMesh.rotation.x ) * 0.05;
             requestAnimationFrame(render);
             renderer.render( scene, camera );
         }
     }  
-
-
-    //usingWebGLRenderer();  
-    function usingWebGLRenderer(){
-
-        scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000);
-        camera.position.z = 150;
-
-        // Add light to scene
-        addLight();
-        
-        // Extrude shape ( Batman logo) 
-        var svgString = $("#batman-path").attr("d"),
-            extrudeGeometry = new THREE.ExtrudeGeometry(transformSVGPathExposed(svgString),{
-                    amount: 2,
-                    bevelThickness: 2,
-                    bevelSize :0.5,
-                    bevelEnabled: true,
-                    bevelEnabled : true,
-                    bevelSegments: 3,
-                    curveSegments : 12,
-                    steps: 1
-                }
-            ),
-            extrudeMeshMaterial = new THREE.MeshPhongMaterial({color: 0x333333});
-
-        extrudeGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(-390, -74, 0));
-        extrudeMesh = new THREE.Mesh(extrudeGeometry, extrudeMeshMaterial);
-        extrudeMesh.scale.x = 0.1;
-        extrudeMesh.scale.y = 0.1;
-        extrudeMesh.rotation.z = Math.PI;
-        extrudeMesh.rotation.x = -1.1;
-        scene.add(extrudeMesh);
-
-        function addLight(){
-            spotLight = new THREE.SpotLight( 0xffffff );
-            spotLight.position.set(0, 0, 500 );
-            scene.add(spotLight);
-        }
-        
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
-
-        // Add OrbitControls so that we can pan around with the mouse.
-        orbitControl = new THREE.OrbitControls(camera, renderer.domElement);
-
-
-        $("#batman-logo").append(renderer.domElement);
-        var previousPoint = {
-            x : window.pageX,
-            y : window.pageY
-        };
-
-        render();
-            
-        function render() {
-            requestAnimationFrame(render);
-            renderer.render( scene, camera );
-            orbitControl.update();
-        }
-    }
 
 })();
